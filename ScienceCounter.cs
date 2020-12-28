585362d468dd203fe3764820b5bec4a55d110ed8
Codapop <george.dikun91@gmail.com>
2020-12-28 23:11:27 +0800

    Add working random science distribution for experiments that are not automatically sorted.

--- ScienceCounter.cs
+++ ScienceCounter.cs
@@ -24,11 +24,32 @@ namespace SciencePlus
 
         public void ScienceProcessingCallback(float sciValue, ScienceSubject sub, ProtoVessel pv, bool test)
         {
+            Debug.Log("[--------SCIENCE+--------]: " + sub.title);
+            
+            bool hasPlanet = false;
             foreach (ScienceType scienceType in allScienceColors)
             {
                 foreach (string body in scienceType.bodyList)
                 {
                     if (sub.title.Contains(body))
+                    {
+                        float newTotal = scienceType.scienceCache + sub.science;
+                        scienceType.scienceCache = newTotal;
+                        Debug.Log("[--------SCIENCE+--------]: " + scienceType.scienceName + " add to cache: " + sub.science);
+                        Debug.Log("[--------SCIENCE+--------]: " + scienceType.scienceName + " cache: " + scienceType.scienceCache);
+                        hasPlanet = true;
+                        Debug.Log("[--------SCIENCE+--------]: Bool = " + hasPlanet);
+                    }
+                }
+            }
+
+            if (!hasPlanet)
+            {
+                int randomNumber = random.Next(8);
+                Debug.Log("[--------SCIENCE+--------]: RandomNumber = " + randomNumber);
+                foreach (ScienceType scienceType in allScienceColors)
+                {
+                    if (randomNumber == scienceType.randInt)
                     {
                         float newTotal = scienceType.scienceCache + sub.science;
                         scienceType.scienceCache = newTotal;
@@ -41,33 +62,36 @@ namespace SciencePlus
 
         public class ScienceType
         {
-            public ScienceType(string color, List<string> bodyList, float scienceBank = 0, float scienceCache = 0)
+            public ScienceType(string color, List<string> bodyList, int randInt, float scienceBank = 0, float scienceCache = 0)
             {
                 this.color = color;
                 this.scienceName = color + "Science";
                 this.bodyList = bodyList;
+                this.randInt = randInt;
                 this.scienceBank = scienceBank;
                 this.scienceCache = scienceCache;
             }
             public string color;
             public string scienceName;
             public List<string> bodyList;
+            public int randInt;
             public float scienceBank;
             public float scienceCache;
         }
 
         public List<ScienceType> allScienceColors = new List<ScienceType>()
         {
-            new ScienceType("Red",    new List<string>() { "Moho",    "Duna"            }),
-            new ScienceType("Orange", new List<string>() { "Dres",    "Vall"            }),
-            new ScienceType("Yellow", new List<string>() { "Mun",     "Pol"             }),
-            new ScienceType("Green",  new List<string>() { "Minmus",  "Ike"             }),
-            new ScienceType("Blue",   new List<string>() { "Kerbin",  "Eeloo"           }),
-            new ScienceType("Purple", new List<string>() { "Eve",     "Bop"             }),
-            new ScienceType("Gold",   new List<string>() { "Kerbol",  "Jool",   "Tylo"  }),
-            new ScienceType("Silver", new List<string>() { "Gilly",   "Laythe"          })
+            new ScienceType("Red",    new List<string>() { "Moho",    "Duna"            }, 0),
+            new ScienceType("Orange", new List<string>() { "Dres",    "Vall"            }, 1),
+            new ScienceType("Yellow", new List<string>() { "Mun",     "Pol"             }, 2),
+            new ScienceType("Green",  new List<string>() { "Minmus",  "Ike"             }, 3),
+            new ScienceType("Blue",   new List<string>() { "Kerbin",  "Eeloo"           }, 4),
+            new ScienceType("Purple", new List<string>() { "Eve",     "Bop"             }, 5),
+            new ScienceType("Gold",   new List<string>() { "Kerbol",  "Jool",   "Tylo"  }, 6),
+            new ScienceType("Silver", new List<string>() { "Gilly",   "Laythe"          }, 7)
         };
 
+        private static readonly System.Random random = new System.Random();
         public static ScienceCounter instance;
     }
 }
