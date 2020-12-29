bad429007d3769c7a0ce5bf29b03199de0934884
Codapop <george.dikun91@gmail.com>
2020-12-30 02:06:42 +0800

    Clean up code and prepare for UI implementaton.

---
+++ RnDButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace SciencePlus
+{
+    [KSPAddon(KSPAddon.Startup.AllGameScenes, false)]
+
+    public class RnDButton : MonoBehaviour
+    {
+        private void Start()
+        {
+            GameEvents.onGUIRnDComplexSpawn.Add(SpawnButton);
+            GameEvents.onGUIRnDComplexDespawn.Add(HideButton);
+        }
+
+        private void OnDestroy()
+        {
+            GameEvents.onGUIRnDComplexSpawn.Remove(SpawnButton);
+            GameEvents.onGUIRnDComplexDespawn.Remove(HideButton);
+        }
+
+        private Rect windowPosition = new Rect();
+
+        private void SpawnButton()
+        {
+
+        }
+
+        private void HideButton()
+        {
+
+        }
+
+    }
+}
