ae1bd1fd3eee2cacb6535e0bcc2e5169235b5003
Codapop <george.dikun91@gmail.com>
2021-02-02 03:33:40 +0800

    Add working science color purchase system.

--- RnDButton.cs
+++ RnDButton.cs
@@ -15,25 +15,69 @@ namespace SciencePlus
 
         private void OnDestroy()
         {
-            //GameEvents.onGUIRnDComplexSpawn.Remove(SpawnButton);
+            GameEvents.onGUIRnDComplexSpawn.Remove(RnDStuff);
+            GameEvents.OnTechnologyResearched.Remove(PressButtonCallback);
         }
 
         private void RnDStuff()
         {
             if (ResearchAndDevelopment.Instance != null)
             {
-                Debug.Log("[--------SCIENCE+--------]: testing 1 2");
-                ResearchAndDevelopment.RefreshTechTreeUI();
-
+                ResearchAndDevelopment.Instance.SetScience(0, TransactionReasons.RnDs);
             }
         }
 
-        private void PressButtonCallback(GameEvents.HostTargetAction<RDTech, RDTech.OperationResult> targetAction)
+        public void PressButtonCallback(GameEvents.HostTargetAction<RDTech, RDTech.OperationResult> targetAction)
         {
-            Debug.Log("[--------SCIENCE+--------]: There's your button!" + targetAction.host.scienceCost);
-            Debug.Log("[--------SCIENCE+--------]: There's your button!" + targetAction.host.techID);
-            Debug.Log("[--------SCIENCE+--------]: There's your button!" + targetAction.host.title);
-            Debug.Log("[--------SCIENCE+--------]: There's your button!" + targetAction.host.host);
+            string color1 = "noColor";
+            string color2 = "noColor";
+
+            ConfigNode TechNode = ConfigNode.Load(KSPUtil.ApplicationRootPath + "GameData/Squad/Resources/TechTree.cfg");
+            TechNode = TechNode.GetNode("TechTree");
+            foreach (ConfigNode RDNode in TechNode.GetNodes("RDNode"))
+            {
+                if (RDNode.GetValue("id") == targetAction.host.techID)
+                {
+                    if (RDNode.GetValue("color1") != null)
+                    {
+                        color1 = RDNode.GetValue("color1");
+                    }
+                    if (RDNode.GetValue("color2") != null)
+                    {
+                        color2 = RDNode.GetValue("color2");
+                    }
+                }
+            }
+
+            if (ResearchAndDevelopment.Instance.GetTechState(targetAction.host.techID) == null)
+            {
+                bool colorcheck = true;
+                foreach (ScienceCounter.ScienceType scienceType in ScienceCounter.instance.allScienceColors)
+                {
+                    if ((color1 == scienceType.color) | (color2 == scienceType.color))
+                    {
+                        if (scienceType.scienceBank < targetAction.host.scienceCost)
+                        {
+                            colorcheck = false;
+                        }
+                    }
+                }
+                if (colorcheck)
+                {
+                    ResearchAndDevelopment.Instance.AddScience(targetAction.host.scienceCost, TransactionReasons.RnDs);
+                }
+            }
+
+            if (ResearchAndDevelopment.Instance.GetTechState(targetAction.host.techID) != null)
+            {
+                foreach (ScienceCounter.ScienceType scienceType in ScienceCounter.instance.allScienceColors)
+                {
+                    if ((color1 == scienceType.color) | (color2 == scienceType.color))
+                    {
+                        scienceType.scienceCache = scienceType.scienceCache - targetAction.host.scienceCost;
+                    }
+                }
+            }
         }
     }
 }
